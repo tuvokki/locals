83a738abb27d6ce6ad42872ef25e1f5526cd1892
Wouter <tuvokki@gmail.com>
2014-12-07 22:44:39 +0100

    Using chance

--- src/javascript/controllers/MainController.js
+++ src/javascript/controllers/MainController.js
@@ -1,22 +1,32 @@
 /**
  * MainController. Responsible for the initial view.
  */
-app.controller('MainController', function($scope) {
-    $scope.whatsMyName = 'Zwadonk';
-    $scope.whosTheMan = 'Okki';
-    $scope.things = [];
-    for (var i = 0; i < 100; i++) {
-        $scope.things.push({
-            code: i
-        });
+app.controller('MainController', function($scope, GithubSvc){
+  $scope.whatsMyName = 'Zwadonk';
+  $scope.whosTheMan = chance.string();
+  $scope.things = [];
+  GithubSvc.fetchStories().success(function (users) {
+    $scope.users = users;
+    console.log("users", users);
+  });
+
+  for (var i = 0; i < 100; i++) {
+    $scope.things.push({code: i});
+  }
+
+  $scope.toggleRightpanel = function(event){
+    $scope.showInfo = !$scope.showInfo;
+    if(event){
+      event.stopPropagation();
+      event.preventDefault();
     }
+  };
+});
 
-    $scope.toggleRightpanel = function(event) {
-        $scope.showInfo = !$scope.showInfo;
-        if (event) {
-            event.stopPropagation();
-            event.preventDefault();
-        }
-    };
-
+app.factory('GithubSvc', function ($http) {
+  return {
+    fetchStories: function () {
+      return $http.get('https://api.github.com/users');
+    }
+  };
 });
